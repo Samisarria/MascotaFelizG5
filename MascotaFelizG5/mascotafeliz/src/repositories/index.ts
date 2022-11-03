32fd7547312f95ddb2d705ef79c1a723752a65c9
Guillermo Fonseca <guillermo01.fonseca@gmail.com>
2022-11-03 00:01:07 -0500

    Actualizacion Proyecto - Autenticacion

--- MascotaFelizG5/mascotafeliz/src/repositories/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './mascota.repository';
-export * from './plan.repository';
-export * from './productos-yservicios.repository';
-export * from './prospecto.repository';
-export * from './sucursal.repository';
-export * from './usuario.repository';
