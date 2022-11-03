7b52ee98e6014cfebd522d5e589f8f48e4bf74c6
Guillermo Fonseca <guillermo01.fonseca@gmail.com>
2022-11-03 00:34:52 -0500

    Se eliminan archivos duplicados

--- dist/controllers/usuario.controller.d.ts
+++
@@ -1,9 +0,0 @@
-import { Usuario } from '../models';
-import { UsuarioRepository } from '../repositories';
-import { AutenticacionService } from '../services';
-export declare class UsuarioController {
-    usuarioRepository: UsuarioRepository;
-    servicioAutenticacion: AutenticacionService;
-    constructor(usuarioRepository: UsuarioRepository, servicioAutenticacion: AutenticacionService);
-    create(usuario: Omit<Usuario, 'id'>): Promise<Usuario>;
-}
--- dist/index.d.ts
+++
@@ -1,3 +0,0 @@
-import { ApplicationConfig, MascotasApplicacion } from './application';
-export * from './application';
-export declare function main(options?: ApplicationConfig): Promise<MascotasApplicacion>;
