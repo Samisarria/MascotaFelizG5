514ecba92eaa562e146bbc4591180d96d29d7c73
Guillermo Fonseca <guillermo01.fonseca@gmail.com>
2022-11-08 23:17:57 -0500

    Generacion Proyecto Angular

---
+++ Frontend_MascotaFelizG5/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css','../shared/css/bootstrap.min.css',
+  '../shared/css/font-awesome.min.css','../shared/css/nivo-lightbox.css',
+  '../shared/css/nivo_themes/default/default.css','../shared/css/templatemo-style.css']
+})
+export class AppComponent {
+  title = 'MascotaFelizG5';
+}
---
+++ Frontend_MascotaFelizG5/src/app/modulos/sucursales/sucursales.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { SucursalesRoutingModule } from './sucursales-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    SucursalesRoutingModule
+  ]
+})
+export class SucursalesModule { }
