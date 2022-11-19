c4e9b56df5fc2619563ebf625ae3164084196ba9
Guillermo Fonseca <guillermo01.fonseca@gmail.com>
2022-11-18 23:01:06 -0500

    Ultimos cambios

--- Frontend_MascotaFelizG5/src/app/modulos/productoservicios/consultar/consultar.component.ts
+++ Frontend_MascotaFelizG5/src/app/modulos/productoservicios/consultar/consultar.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { ModeloIdentificar } from 'src/app/modelos/Identificar.modelo';
 import { ModeloProductoServicio } from 'src/app/modelos/productoservicio.model';
 import { ProductoserviciosService } from 'src/app/servicios/productoservicios.service';
+import { SeguridadService } from 'src/app/servicios/seguridad.service';
 
 @Component({
   selector: 'app-consultar',
@@ -10,12 +13,17 @@ import { ProductoserviciosService } from 'src/app/servicios/productoservicios.se
 })
 export class ConsultarComponent implements OnInit {
 
+  rolUsuario?: string = '';
+  subs: Subscription = new Subscription();
   listadoProductosServicios: ModeloProductoServicio[] = [];
 
-  constructor(private productoServicioService: ProductoserviciosService, private router: Router) { }
+  constructor(private productoServicioService: ProductoserviciosService, private router: Router, private seguridadServicio: SeguridadService,) { }
 
   ngOnInit(): void {
     this.ObtenerListadoProductosServicios();
+    this.subs = this.seguridadServicio.ObtenerDatosUsuarioEnSesion().subscribe((datos:ModeloIdentificar) => {
+      this.rolUsuario = datos.datos?.rol;
+    });
   }
 
   AgregarProductoServicio() {
